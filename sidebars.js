4ccbfd2af5a9ec92ab898aebc2e658f66e64501c
Yomi Eluwande <yomi.eluwande@gmail.com>
2025-07-10 13:07:35 +0200

    update link in sidebar

--- sidebars.js
+++ sidebars.js
@@ -87,7 +87,7 @@ const sidebars = {
         "single-sign-on",
         "user-roles",
         "inverted-call-stacks",
-        "flamecharts-with-cpu-profiles",
+        "flame-charts-with-cpu-profiles",
       ],
     },
     {
